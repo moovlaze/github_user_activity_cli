6f7223e6d5edd8732989809e76a42f6faeed58ab
Moovlaze <max.moovlaze@gmail.com>
2025-01-31 16:53:31 +0300

    add CLI possibilities

--- main.py
+++ main.py
@@ -1,52 +1,41 @@
 import requests
+import argparse
 
-r = requests.get('https://api.github.com/users/moovlaze/events')
-
-response = r.json()
-
-for resp in reversed(response):
-
-  if resp.get('type') == 'CreateEvent':
-    # events_dict.setdefault(resp.get('type'), []).append(
-    # {
-    #   "ref_type": resp.get('payload').get('ref_type'),
-    #   "repo": resp.get('repo').get('name'),
-    #   "ref": resp.get('payload').get('ref'),
-    #   "created_at": resp.get('created_at')
-    # })
-    print(f'- User CREATED {resp.get('payload').get('ref_type')} {resp.get('repo').get('name')}')
-
-  if resp.get('type') == 'PushEvent':
-    # events_dict.setdefault(resp.get('type'), [])
-
-    # events_dict[resp.get('type')].append({
-    #   "repo": resp.get('repo').get('name'),
-    #   "commits": len(resp.get('payload').get('commits'))
-    # })
-    # repo_name = resp.get('repo').get('name')
-    # commit_count = 0
-
-    # if resp.get('repo').get('name') == repo_name:
-    #   commit_count += 1
-    # else:
-    #    repo_name = resp.get('repo').get('name')
-
-    print(f'- User PUSH {len(resp.get('payload').get('commits'))} commits to {resp.get('repo').get('name')}')
-
-  if resp.get('type') == 'WatchEvent':
-    print(f'- User STARRED {resp.get('repo').get('name')}')
-
-    
-# print(events_dict)
-
-# for item in events_dict['CreateEvent']:
+def view_activity(user_name):
   
-#   if item['ref_type'] == 'repository':
-#     print(f'- Создал репозиторий  {item['repo']} в {item['created_at']}')
+  r = requests.get(f'https://api.github.com/users/{user_name}/events')
 
-#   if item['ref_type'] == 'branch':
-#     print(f'\t- Создал ветку  {item['ref']} в {item['created_at']} \n')
+  response = r.json()
+  
+  if type(response) == dict and response.get('status') == '404':
+    print('User not found')
+    return
 
-# for item in events_dict['PushEvent']:
+  commits_count = 0
 
-#   print(item)
+  for resp in reversed(response):
+
+    if resp.get('type') == 'CreateEvent':
+      print(f'- User CREATED {resp.get('payload').get('ref_type')} {resp.get('repo').get('name')}')
+      if resp.get('payload').get('ref_type') == 'branch':
+        print(f'  - User PUSH {commits_count} commits to {resp.get('repo').get('name')}')
+        commits_count = 0
+
+    if resp.get('type') == 'PushEvent':
+      print(f'- User PUSH {len(resp.get('payload').get('commits'))} commits to {resp.get('repo').get('name')}')
+
+    if resp.get('type') == 'WatchEvent':
+      print(f'- User STARRED {resp.get('repo').get('name')}')
+      
+      
+if __name__ == '__main__':
+  
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('user_name')
+
+  args = parser.parse_args()
+
+  view_activity(args.user_name)
+
+
